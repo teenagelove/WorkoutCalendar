836dd5b8d37026dab0c307e84e2a87f87af44e5c
Danil Kazakov <dani4upa586@gmail.com>
2025-12-11 20:14:04 +0900

    chore: initialize project

---
+++ WorkoutCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutCalendar
+//
+//  Created by Danil Kazakov on 2025/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutCalendar/WorkoutCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutCalendarApp.swift
+//  WorkoutCalendar
+//
+//  Created by Danil Kazakov on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
